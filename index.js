d37314f969e1b63bfc1a1ab722d394cfa11fbf35
zdog32 <matt.thew.zhou@gmail.com>
2024-10-21 16:15:33 -0400

    disallow awkward work endings

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 (function (global) {
     var consonants = ['b','c','d','f','g','h','j','k','l','m','n','p','q','qu','r','s','t','v','w','x','y','z','bl','br','ch','ck','cl','cr','dr','fl','fr','gl','gr','kl','kr','pl','pr','sch','sh','shr','sl','st','sz','tch','th','thr','tr','ts','vl','vr','wr'];
+    var consonants_nolast = ['qu','bl','br','cl','cr','dr','fl','fr','gl','gr','kl','kr','pl','pr','shr','sl','thr','tr','vr','wr']
     var vowels = ['a','e','i','o','u','y','ae','ee','ie','ei','ea','ou','oo','ai','ey','ao','ay','oy','aa','oi','eu'];
     var wordElement = document.querySelector("#word");
     var numberInput = document.querySelector("#numberBox");
@@ -8,16 +9,26 @@
         var i = Math.floor(Math.random()*consonants.length);
         return consonants[i];
     }
+    
+    function getConsonantNoLast(){
+        var i = Math.floor(Math.random()*consonants_nolast.length);
+        return consonants_nolast[i];
+    }
+    
     function getVowel(){
         var i = Math.floor(Math.random()*vowels.length);
         return vowels[i];
     }
+    
     function makeWord (length){
         var result = "";
         for (var i = 0; i < length; i++) {
-            if(i%2 == 0){
+            if(i%2 == 0 && i != length){
                 //even
                 result = result + getConsonant();
+            }
+            else if(i%2 == 0 && i == length){
+                getConsonantNoLast();
             }
             else{
                 //odd
